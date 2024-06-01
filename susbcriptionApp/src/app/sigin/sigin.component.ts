982683821ecd85a280be4d305641b77f66b71c35
Dafatalk <dafa345.daf@gmail.com>
2024-06-01 01:18:59 -0500

    TODO funcionando relativamente

--- susbcriptionApp/src/app/sigin/sigin.component.ts
+++ susbcriptionApp/src/app/sigin/sigin.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sigin',
   templateUrl: './sigin.component.html',
   styleUrls: ['./sigin.component.css']
 })
-export class SiginComponent implements OnInit {
+export class SiginComponent {
+  name: string = '';
+  lastName: string = '';
+  mobileNumber: string = '';
+  documentType: string = '';
+  identityDocument: string = '';
+  email: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  sigin() {
+    // Aquí puedes manejar la lógica de registro
+    console.log(this.name, this.lastName, this.mobileNumber, this.documentType, this.identityDocument, this.email);
+  }
 }
