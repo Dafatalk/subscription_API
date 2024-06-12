89e6eab7b160e409ff83ad4d1b3dcbbc0f9621c9
Diaz <diazjulian460@gmail.com>
2024-06-12 11:06:48 -0500

    Se arregla la barbaridad que hizo mi compa el dafa

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/subscriptionapp'),
+      dir: require('path').join(__dirname, './coverage/susbcription-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
